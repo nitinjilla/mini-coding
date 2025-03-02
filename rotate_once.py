9a5bd2d1c96e4979355907457d6dadcdf9f3a94f
nitinjilla <nitinjilla9@gmail.com>
2025-03-02 11:51:31 +0000

    first commit

---
+++ rotate_once.py
@@ -0,0 +1,18 @@
+def rotate_once(input_list: list) -> list:
+    first_element = input_list[0]
+
+    for i in range (1, len(input_list)):
+        input_list[i-1] = input_list[i]
+    
+    input_list[len(input_list) - 1] = first_element
+
+    return input_list
+
+l = [1, 2, 3, 4, 5]
+print(rotate_once(l))
+print(rotate_once(l))
+print(rotate_once(l))
+print(rotate_once(l))
+
+
+
